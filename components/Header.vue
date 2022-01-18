e4461f5e27b57de218f30cd3738289f63d12baa5
Ihor Bodnarchuk <ihorbond@gmail.com>
2022-01-17 18:47:20 -0600

    init

---
+++ components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+        <b-navbar-brand href="https://zerocodenft.com">
+            Zero Code NFT
+        </b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item to="/#roadmap">Roadmap</b-nav-item>
+                <b-nav-item to="/#team">Team</b-nav-item>
+            </b-navbar-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+                <b-button
+                    variant="light"
+                    :disabled="!!$wallet.account"
+                    @click="$wallet.connect">
+                    <strong>{{
+                        !!$wallet.account ? $wallet.accountCompact : 'Connect Wallet'
+                    }}</strong>
+                </b-button>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+</template>
